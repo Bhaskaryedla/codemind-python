5495ff772dbea4887ffb49030e9fed67d56dfca3
Udaya Sai bhaskar <106659912+Bhaskaryedla@users.noreply.github.com>
2022-09-18 13:00:37 +0530

    n=int(input()) while(True):     if n%2==0:         n//=2     elif n%3==0:         n//=3     elif n%5==0:         n//=5     elif n==1:         print("Ugly Number")         break     else:         print("Not Ugly Number")         break

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,17 +1,14 @@
-m=int(input())
+n=int(input())
 while(True):
-    if m%2==0:
-        m//=2
-    elif m%3==0:
-        m//=3
-    elif m%5==0:
-        m//=5
-    elif m==1:
+    if n%2==0:
+        n//=2
+    elif n%3==0:
+        n//=3
+    elif n%5==0:
+        n//=5
+    elif n==1:
         print("Ugly Number")
         break
     else:
         print("Not Ugly Number")
         break
-    
-    
-        
